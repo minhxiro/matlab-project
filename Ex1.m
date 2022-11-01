8b386ff819bfb5a9b94a985989d70c5d275d5a24
shirin44 <117139459+shirin44@users.noreply.github.com>
2022-11-01 11:50:53 +0700

    Update Ex1.m

--- Ex1.m
+++ Ex1.m
@@ -1,11 +1,13 @@
-%% Ex2
+%% week 2 
 % Determine the input
-d = input('Enter the distance: ');
-g = 9.8;
-theta = pi / 4;
+d = 183; %distance of the narrowest point of the grand canyon
+g = 9.8; % gravity
+theta = pi / 4; % the angle of launch
 
 % Calculation to find the initial velocity
 v0 = sqrt((d * g) / sin(2 * theta) );
 
 % Convert m/s into km/h
-fprintf('The initial velocity is: %1.3f', v0);
+new_v0= v0*3.6;
+fprintf('The initial velocity is: %1.2f in km/h', new_v0);
+
