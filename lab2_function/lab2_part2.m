e8eac884716689a8c934a39f99bb1e175c9656a2
The Rendezvous 2022: Néonize <94245285+minhxiro@users.noreply.github.com>
2022-11-28 00:30:52 +0700

    Update lab2_part2.m

--- lab2_function/lab2_part2.m
+++ lab2_function/lab2_part2.m
@@ -1,7 +1,8 @@
 %%Lab2
-img = imread('coloredChips.png');
-figure(1), imshow(img);
+img = imread('coloredChips.png');%importing image 
+figure(1), imshow(img);%displaying original image(pre adjustment)
 %Create menu for color selection
+disp('Welcome the the color conversion program, please select the color to be converted') %Introduction to the program
 disp('The list of colors to be converted:')
 disp('r for red');
 disp('g for green');
@@ -11,7 +12,8 @@ disp('c for cyan');
 disp('o for orange');
 disp('m for magneta');
 disp('w for white');
-color_seletion = input('Enter the color you want to change to black: ', 's');
-image_row = size(img, 1);
-image_col = size(img, 2);
-figure(2), imshow(color_conversion(img, color_seletion));
+color_seletion = input('Enter the color you want to change to black: ', 's');%prompting user to select color
+image_row = size(img, 1);%get rows of the image
+image_col = size(img, 2);%get coloumns of image
+figure(2), imshow(color_conversion(img, color_seletion));%Show the image after using color conversion function
+title('Color Conversion Program');%Add the title for the program
